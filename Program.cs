cd33ef515ab60a7405a2f565d6b0493f95f5c2eb
Orsós Lívia <orsoslivia@hotmail.com>
2021-04-12 10:25:28 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using Scriban;
+
+namespace html
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = "template.sbn";
+            var content = File.ReadAllText(fileName);
+            var template = Template.Parse(content, fileName);
+            var result = template.Render(new { UserName = "Gábor" });
+            Console.WriteLine(result);
+        }
+    }
+}
