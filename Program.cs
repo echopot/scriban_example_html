566cfbe1d0f3118336bfbe4f67b54044711dcadc
Orsós Lívia <orsoslivia@hotmail.com>
2021-04-12 11:18:25 +0200

    html template

--- Program.cs
+++ Program.cs
@@ -6,12 +6,25 @@ namespace html
 {
     class Program
     {
+        class Item
+        {
+            public string name { get; set; }
+            public int quantity { get; set; }
+        }
+        static Item[] items = { new Item { name = "milk", quantity = 1 },
+                                new Item { name = "potato", quantity = 2 },
+                                };
         static void Main(string[] args)
         {
+            
             var fileName = "template.sbn";
             var content = File.ReadAllText(fileName);
             var template = Template.Parse(content, fileName);
-            var result = template.Render(new { UserName = "Gábor" });
+            var result = template.Render(new { 
+                UserName = "User Name",
+                Address = "1 Smith St, Smithville",
+                Items = items
+            });
             Console.WriteLine(result);
         }
     }
